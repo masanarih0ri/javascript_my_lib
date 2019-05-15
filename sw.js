c4d7bae7a6cb5161342c0e54cc2c82e2a3e3168a
m-hori <m-hori@m3career.com>
2019-05-15 17:49:20 +0900

    installイベントの追加

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
+const CACHE_NAME = 'v1'
+
+const FILES_TO_CACHE = [
+  '/serviceworker.html',
+];
+
 self.addEventListener('install', function(event) {
-  console.log('install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
