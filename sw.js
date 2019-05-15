604c9e8106208231b3e46ef0ca37fc71268c03ca
masanarih0ri <masanarihoridevelop@gmail.com>
2019-05-16 00:16:09 +0900

    activateの処理を追加

--- sw.js
+++ sw.js
@@ -6,11 +6,27 @@ const FILES_TO_CACHE = [
   '/javascript_my_lib/js/materialize.js'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// activateイベントを使って古いキャッシュをクリーンアップする
+// App Shellファイルのいずれかが変更されるたびにservice workerがそのキャッシュを更新するようにする
+// これを機能させるには、CACHE_NAME変数を増やす必要がある
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if(key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
