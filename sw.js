24c2d627a83e859122e8ab79598721b0431efdff
m-hori <m-hori@m3career.com>
2019-05-15 18:07:14 +0900

    キャッシュファイルの部分にエラーがあったので修正

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'v1'
 
 const FILES_TO_CACHE = [
   '/javascript_my_lib/serviceworker.html',
-  '/javascript_my_lib/css/materialize.css'.
+  '/javascript_my_lib/css/materialize.css',
   '/javascript_my_lib/js/materialize.js'
 ];
 
