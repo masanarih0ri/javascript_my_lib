bab45407eec1cdf7f68a222935cf633e1f42cb6a
m-hori <m-hori@m3career.com>
2019-05-16 15:39:23 +0900

    cacheの更新処理を追加

--- sw.js
+++ sw.js
@@ -1,37 +1,82 @@
-const CACHE_NAME = 'v1'
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 
+// app shellになりうるファイルをいれることも考えられる
 const FILES_TO_CACHE = [
-  '/javascript_my_lib/serviceworker.html',
-  '/javascript_my_lib/css/materialize.css',
-  '/javascript_my_lib/js/materialize.js'
+  '/serviceworker.html',
+  '/css/materialize.css',
+  '/js/materialize.js'
 ];
 
 self.addEventListener('install', (event) => {
+  // ServiceWorkerはwaitUntil内のコードが実行されるまでinstallされない
   event.waitUntil(
+    // cachesは特定のServiceWorkerの範囲内でデータを保存を可能にする特別なCacheStorageオブジェクト。ServiceWorkerではCacheAPIを利用する
+    // このコードではcacheを開き、アプリが使用する全てのファイルをキャッシュに追加する
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
 // activateイベントを使って古いキャッシュをクリーンアップする
 // App Shellファイルのいずれかが変更されるたびにservice workerがそのキャッシュを更新するようにする
 // これを機能させるには、CACHE_NAME変数を増やす必要がある
 self.addEventListener('activate', (event) => {
-  // event.waitUntil(
-  //   caches.keys().then((keyList) => {
-  //     return Promise.all(keyList.map((key) => {
-  //       if(key !== CACHE_NAME) {
-  //         console.log('[ServiceWorker] Removing old cache', key);
-  //         return caches.delete(key);
-  //       }
-  //     }));
-  //   })
-  // );
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+          console.log('delete!');
+        }
+      }));
+    })
+  );
   console.log('activated!');
 });
 
+// fetchイベント
 self.addEventListener('fetch', (event) => {
-  console.log(event.request.url);
+  console.log('fetch!');
+  console.log(event.request.mode);
+  console.log('Fetch intercepted for:', event.request.url);
+  // event.respondWith(
+  //   caches.match(event.request)
+  //     .then((response) => {
+  //       if(response) {
+  //         return response;
+  //       }
+  //
+  //       return fetch(event.request)
+  //       .then((response) => {
+  //         // レスポンスがない場合（オフラインの場合）
+  //         if(!response || response.status !== 200 || response.type !== 'basic') {
+  //           return response;
+  //         }
+  //
+  //         let responseTocache = response.clone();
+  //
+  //         caches.open(CACHE_NAME)
+  //         .then((cache) => {
+  //           cache.put(event.request, responseTocache);
+  //         });
+  //         return response;
+  //       });
+  //     })
+  //   );
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
